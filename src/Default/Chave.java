f35eaffde5106b0af3e8ea2564cceac65aba2725
Paulo Tobias <paulohtobias@outlook.com>
2017-06-12 21:31:13 -0300

    Remoção da classe Localizavel e remoção de erros consequentes
    Classe Localizavel foi removida. Outras mudanças que foram feitas (arquivos MUDANÇAS.txt)
    
    	* A função public ArrayList<Chave> inicializaChaves(ArrayList<Sala> salas) foi movida para a classe Sala.
    		* Cada sala, se tem algum aporta trancada, terá 1 chave. Se a sala não tem portas trancadas, então não tem chaves.
    	* Funções do tipo getItemDisponivelSala serão movidos para a classe Sala.
    	* Funções para printar os itens da sala serão movidas para a classe Sala.
    
    	* Funções para soltar o Item serão movidas para a classe Jogador.
    
    	* No máximo 5 salas aleatórias tinham machado / ouro.

--- src/Default/Chave.java
+++ src/Default/Chave.java
@@ -5,77 +5,18 @@ import java.util.ArrayList;
 
 public class Chave extends Pegavel{
 	
-	private boolean usada = false;
-	
-	
-	public Chave() {
-		this.setPeso(1);
-	}
-	
-	public boolean isUsada() {
-		return usada;
-	}
+    private boolean usada = false;
 
 
-	public void setUsada(boolean usada) {
-		this.usada = usada;
-	}
-	
-	
-	public void soltaChave(Sala sala){
-		if(!this.isUsada()){
-			this.setLocalizacao(sala);
-			this.setPegado(false);
-		}
-		
-		
-	}
-	
-	public static ArrayList<Chave> setPegadoChaveSala(ArrayList<Chave> chaves,Sala sala){
-		for(int i=0;i<chaves.size();i++){
-			if(chaves.get(i).getLocalizacao().getNome() == sala.getNome()){
-				chaves.get(i).setPegado(true);
-			}
-		}
-		return chaves;
-	}
-	
-	public static Chave getChaveDisponivelSala(ArrayList<Chave> chaves,Sala sala){
-		for(Chave chave:chaves){
-			if(chave.isPegado() == false && chave.getLocalizacao().getNome() == sala.getNome()){				
-				
-				return chave;
-				
-			}
-		}
-		return null;
-	}
-	
+    public Chave() {
+        this.setPeso(1);
+    }
 
-	public static int imprimeChavesNaSala(ArrayList<Chave> chaves,Sala sala){
-		int quantidade = 0;
-		for(Chave chave:chaves){
-			if(chave.isPegado() == false && chave.isUsada() == false && chave.getLocalizacao().getNome() == sala.getNome()){
-				System.out.println("chave");
-				quantidade++;
-			}
-		}		
-		return quantidade;
-	}
-	public ArrayList<Chave> inicializaChaves(ArrayList<Sala> salas){
-		ArrayList<Chave> chaves = new ArrayList<Chave>();
-		//colocar as chaves em suas devidas salas
-		for (Sala sala : salas) {
-			//solta uma chave para cada porta fechada
-			for (Porta porta : sala.getPortas()) {
-				if(!porta.getAberta()){
-					Chave chave = new Chave();
-					chave.setLocalizacao(sala);
-					chaves.add(chave);
-					break; //limite de 1 chave para ele ter q escolher uma porta para navegar
-				}
-			}
-		}		
-		return chaves;
-	}
+    public boolean isUsada() {
+        return usada;
+    }
+
+    public void setUsada(boolean usada) {
+        this.usada = usada;
+    }
 }
--- src/Default/Pocao.java
+++ src/Default/Pocao.java
@@ -2,69 +2,19 @@ package Default;
 
 import java.util.ArrayList;
 
-public class Pocao extends Pegavel{
-	
-	private boolean utilizado = false;
-	
-	
-	public boolean isUtilizado() {
-		return utilizado;
-	}
+public class Pocao extends Pegavel {
 
+    private boolean utilizado = false;
+    
+    public Pocao() {
+        this.setPeso(1);
+    }
 
-	public void setUtilizado(boolean utilizado) {
-		this.utilizado = utilizado;
-	}
+    public boolean isUtilizado() {
+        return utilizado;
+    }
 
-
-	public Pocao() {
-		this.setPeso(1);
-	}
-	public static ArrayList<Pocao> setPegadoPocaoSala(ArrayList<Pocao> pocoes,Sala sala){
-		for(int i=0;i<pocoes.size();i++){
-			if(pocoes.get(i).getLocalizacao().getNome() == sala.getNome()){
-				pocoes.get(i).setPegado(true);
-			}
-		}
-		return pocoes;
-	}
-	
-	public static Pocao getPocaoDisponivelSala(ArrayList<Pocao> pocoes,Sala sala){
-		for(Pocao pocao:pocoes){
-			if(pocao.isPegado() == false && pocao.getLocalizacao().getNome() == sala.getNome()){								
-				return pocao;				
-			}
-		}
-		return null;
-	}
-	public void soltaPocao(Sala sala){
-		if(!this.isUtilizado()){
-			this.setLocalizacao(sala);
-			this.setPegado(false);
-		}				
-	}
-	
-	
-	public ArrayList<Pocao> inicializaPocao(ArrayList<Sala> salas){
-		ArrayList<Pocao> pocoes = new ArrayList<Pocao>();
-		int quantidade_salas = 5;//quantidade de salas que terá poção
-		ArrayList<Integer> jaAdicionados = new ArrayList<Integer>();
-		for(int i=0;i<quantidade_salas;i++){
-			Pocao pocao = new Pocao();			
-			boolean encontrou = false;
-			Integer pos = new Integer(0);
-			while(!encontrou){
-				pos = new Integer(random.nextInt(salas.size()-1));
-				if(!jaAdicionados.contains(pos)){
-					encontrou = true;
-					break;
-				}
-			}				
-			pocao.setLocalizacao(salas.get(pos.intValue()));							
-			pocoes.add(pocao);
-		}
-		return pocoes;
-	}
-	
-	
+    public void setUtilizado(boolean utilizado) {
+        this.utilizado = utilizado;
+    }
 }
